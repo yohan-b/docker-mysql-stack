3d358a88c23914f455c13e688ac1f16d9ab37d92
yohan <783b8c87@scimetis.net>
2019-08-19 11:45:26 +0200

    Fixed crontab permissions.

--- start_or_update.sh
+++ start_or_update.sh
@@ -2,6 +2,7 @@
 test -f ~/secrets.tar.gz.enc || curl -o ~/secrets.tar.gz.enc https://cloud.scimetis.net/s/$KEY/download
 openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | tar -zxv --strip 2 secrets/docker-mysql-stack/crontab secrets/docker-mysql-stack/debian.cnf
 sudo chown root. crontab debian.cnf
+sudo chmod 644 crontab
 # --force-recreate is used to recreate container when crontab file has changed
 unset VERSION_MYSQL VERSION_CRON
 VERSION_MYSQL=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-mysql.git| head -1 | cut -f 1|cut -c -10) \
